62e7fcf50f93d835ed0e4a04f8443f2b3eb6f51c
Adrian Vergara <adrianvergara22@gmail.com>
2016-11-24 12:01:29 -0500

    Version Estable Commit Ricardo

---
+++ BLL/ClienteBLL.cs
@@ -0,0 +1,130 @@
+﻿using DAL;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ClienteBLL
+    {
+        Respuesta respuesta = new Respuesta();
+        Contexto db = new Contexto();
+
+        public Respuesta Insertar(ClienteDTO cliente)
+        {
+            using(db = new Contexto())
+            {
+                try
+                {
+                    // preparar el cliente para guardar
+                    Cliente c = new Cliente();
+                    c.ClienteId = cliente.ClienteId;
+                    c.Direccion = cliente.Direccion;
+                    c.Nombre = cliente.Nombre;
+                    c.Telefono = cliente.Telefono;
+                    db.Clientes.Add(c);
+                    
+                    // preparar la respuesta
+                    respuesta.FilasAfectadas = db.SaveChanges();
+                    respuesta.Mensaje = "Se realizó la operación satisfactoriamente";
+                    respuesta.Error = false;
+                }
+                catch (System.Data.Entity.Validation.DbEntityValidationException ex)
+                {
+                    respuesta.Mensaje = ex.Message;
+                    respuesta.FilasAfectadas = 0;
+                    respuesta.Error = true;
+                }
+                catch (Exception ex)
+                {
+                    respuesta.Mensaje = ex.Message;
+                    respuesta.FilasAfectadas = 0;
+                    respuesta.Error = true;
+                }
+
+                return respuesta;
+            }
+        }
+
+        public List<ClienteDTO> GetRecords()
+        {
+            using(Contexto db = new Contexto())
+            {
+                return db.Clientes
+                    .Select(t => 
+                        new ClienteDTO 
+                        {
+                            ClienteId = t.ClienteId,
+                            Nombre = t.Nombre,
+                            Direccion = t.Direccion,
+                            Telefono = t.Telefono
+                        }
+                    ).ToList();
+            }
+        }
+
+        public async Task<Respuesta> Delete(int clienteId)
+        {
+
+            try
+            {
+                var user = await db.Clientes.FindAsync(clienteId);
+                if (user == null)
+                {
+                    respuesta.Error = true;
+                    respuesta.FilasAfectadas = 0;
+                    respuesta.Mensaje = "El Usuario no exixte";
+                }
+
+                db.Clientes.Remove(user);
+                // preparar la respuesta
+                respuesta.FilasAfectadas = db.SaveChanges();
+                respuesta.Mensaje = "Se realizó Elimino el usuario satisfactoriamente";
+                respuesta.Error = false;
+            }
+            catch (System.Data.Entity.Validation.DbEntityValidationException ex)
+            {
+                respuesta.Mensaje = ex.Message;
+                respuesta.FilasAfectadas = 0;
+                respuesta.Error = true;
+            }
+            catch (Exception ex)
+            {
+                respuesta.Mensaje = ex.Message;
+                respuesta.FilasAfectadas = 0;
+                respuesta.Error = true;
+            }
+
+            return respuesta;
+
+        }
+
+        public async Task<Respuesta> Update(ClienteDTO cliente)
+        {
+            var user = await db.Clientes.FindAsync(cliente.ClienteId);
+            Respuesta respuesta = new Respuesta();
+            if (user != null)
+            {
+                user.Direccion = cliente.Direccion;
+                user.Telefono = cliente.Telefono;
+                user.Nombre = cliente.Nombre;
+
+                db.Clientes.AddOrUpdate(user);
+                
+                respuesta.FilasAfectadas = db.SaveChanges();
+                respuesta.Mensaje = "Información Actualizada";
+                respuesta.Error = false;
+                return respuesta;
+            }
+
+            respuesta.FilasAfectadas = 0;
+            respuesta.Mensaje = "Información No Actualizada";
+            respuesta.Error = true;
+            return respuesta;
+        }
+    }
+}
---
+++ PPTConsola/Program.cs
@@ -0,0 +1,48 @@
+﻿using Entities;
+using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPTConsola
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+             //7cliente para guardar
+            ClienteDTO cliente1 = new ClienteDTO
+            {
+                Nombre = "Miguel",
+                Direccion = "Alamos 1",
+                Telefono = "3113298374"
+            };
+
+            // guardar el cliente
+            ClienteBLL clienteBLL = new ClienteBLL();
+            Respuesta respuesta = clienteBLL.Insertar(cliente1);
+            Console.WriteLine(respuesta.Mensaje + ", Filas afactadas: " + respuesta.FilasAfectadas);
+            Console.ReadKey();
+
+            // ahora una consulta de los datos
+            Console.WriteLine("Consulta de datos");
+            foreach (var cliente in clienteBLL.GetRecords())
+            {
+                Console.WriteLine(cliente.Nombre + " " + cliente.Telefono);
+            }
+            Console.ReadKey();
+
+            //ProyectoDTO proyecto1 = new ProyectoDTO
+            //{
+            //    Nombre = "Miguel",
+            //    Valor = 10000,
+            //    FechaInicio = ,
+            //    Plazo = "Miguel",
+            //    Estado = "Alamos 1",
+            //    ClienteId = "3113298374",
+            //};
+        }
+    }
+}
